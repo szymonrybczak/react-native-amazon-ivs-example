b0e32544e6faba8d561ebe1d901e4404d89cab1d
szymonrybczak <szymon.rybczak@gmail.com>
2023-07-27 11:52:39 +0200

    feat: add simple enter view

--- App.tsx
+++ App.tsx
@@ -1,19 +1,53 @@
-import {View, Text, StyleSheet} from 'react-native';
-import React from 'react';
+import {StyleSheet, SafeAreaView} from 'react-native';
+import React, {useState} from 'react';
+import {Button, Text, TextInput} from 'react-native-paper';
 
 const App = () => {
+  const [streamURL, setStreamURL] = useState('');
+
+  const navigateToWatchStream = () => {
+    console.log('navigateToWatchStream');
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>App</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        label="Stream URL"
+        value={streamURL}
+        onChangeText={setStreamURL}
+        style={styles.input}
+      />
+      <Button
+        style={styles.button}
+        onPress={navigateToWatchStream}
+        buttonColor="#e1d3eb">
+        <Text style={styles.buttonText}>Submit</Text>
+      </Button>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: '40%',
+    alignItems: 'center',
+  },
+  input: {
+    width: '80%',
+  },
+  button: {
+    marginTop: 20,
+    borderRadius: 10,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#49454f',
+    paddingVertical: 8,
+    paddingHorizontal: 40,
   },
 });
 
